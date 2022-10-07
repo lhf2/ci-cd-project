fa0270912244660edd415575a984fb022e048d89
626712309@qq.com <626712309@qq.com>
2022-10-07 19:52:52 +0800

    生产环境多个tag构建测试

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App test"/>
+  <HelloWorld msg="Welcome to Your Vue.js App test v1.0.1"/>
 </template>
 
 <script>
